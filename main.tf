955f3c9fb1538fc77689814d36535fdce4455b67
Arpithabp <93638630+Arpithabp@users.noreply.github.com>
2023-04-29 21:02:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "example" {
   ami = "ami-02eb7a4783e7e9317" 
   instance_type = "t2.micro" 
-  key_name  = "bank.pem" 
+  key_name  = "bank" 
   vpc_security_group_ids= ["sg-039a2d2c31bb690d3"]
   connection {
     type     = "ssh"
