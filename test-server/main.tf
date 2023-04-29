53d35d415d01ace03e669c6588f8ba64802e9252
root <root@ip-172-31-6-142.ap-south-1.compute.internal>
2023-04-29 07:31:06 +0000

    ss

--- test-server/main.tf
+++ test-server/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "example" {
   ami           = "ami-02eb7a4783e7e9317" // set the AMI ID of the image you want to use
   instance_type = "t2.micro" // set the instance type you want to use
   key_name      = "bank.pem" // set the name of the key pair you want to use to SSH into the instance
-  vpc_security_group_ids= ["sg-04fba683f37ca92fd"]
+  vpc_security_group_ids= ["sg-0fd5056ae19b64938"]
   connection {
     type     = "ssh"
     user     = "ubuntu"
