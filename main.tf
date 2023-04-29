0edfdc01e1e89c970368b2387a485b105a9d4271
root <root@ip-172-31-6-142.ap-south-1.compute.internal>
2023-04-29 09:15:35 +0000

    added tf files

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "example" {
-  ami           = "ami-02eb7a4783e7e9317" // set the AMI ID of the image you want to use
-  instance_type = "t2.micro" // set the instance type you want to use
-  key_name      = "bank.pem" // set the name of the key pair you want to use to SSH into the instance
-  vpc_security_group_ids= ["sg-0fd5056ae19b64938"]
+  ami = "ami-02eb7a4783e7e9317" 
+  instance_type = "t2.micro" 
+  key_name  = "bank.pem" 
+  vpc_security_group_ids= ["sg-039a2d2c31bb690d3"]
   connection {
     type     = "ssh"
     user     = "ubuntu"
--- provider.tf
+++ provider.tf
@@ -1,3 +1,5 @@
 provider "aws" {
 region     = "ap-south-1b" 
+access_key= "AKIARTNXBRKHGQY3YA4L"
+secret_key= "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"
 }
