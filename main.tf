7397dd5ca4b1b2b901d7c23cde37ec2ff03f88be
Arpithabp <93638630+Arpithabp@users.noreply.github.com>
2023-05-01 22:36:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,19 @@
 resource "aws_instance" "example" {
   ami = "ami-02eb7a4783e7e9317"
   instance_type = "t2.micro"
-  key_name  = "project"
+  key_name  = "Project"
   vpc_security_group_ids= ["sg-0b846ff95c9978482"]
   connection {
     type     = "ssh"
     user     = "ubuntu"
-    private_key = file("project.pem")
+    private_key = file("Project.pem")
     host     = self.public_ip
   }
   tags = {
     Name = "deploy-server" // set a name for the instance
       }
+ provisioner "local-exec" {
+         command = "echo ${aws_instance.example.public_ip} > inventory"
+}
 }
 
