fd867c06bb8ad5d669c458c1b4102c2264632d58
Arpithabp <93638630+Arpithabp@users.noreply.github.com>
2023-05-02 13:47:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "example" {
-  ami = "ami-02eb7a4783e7e9317"
-  instance_type = "t2.micro"
-  key_name  = "Project"
+  ami           = "ami-02eb7a4783e7e9317" 
+  instance_type = "t2.micro" 
+  key_name      = "Project" 
   vpc_security_group_ids= ["sg-0b846ff95c9978482"]
   connection {
     type     = "ssh"
@@ -10,10 +10,10 @@ resource "aws_instance" "example" {
     host     = self.public_ip
   }
   tags = {
-    Name = "deploy-server" // set a name for the instance
-      }
- provisioner "local-exec" {
+    Name = "deploy-server" 
+  }
+    provisioner "local-exec" {
          command = "echo ${aws_instance.example.public_ip} > inventory"
-}
-}
 
+  }
+}
